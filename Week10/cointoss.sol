4dd56c0f9483468ac43a78ea6e970a2a676bbda3
Yong Quan <43258778+teo089081@users.noreply.github.com>
2020-04-05 14:42:49 +0800

    finished q2

--- Week10/cointoss.sol
+++ Week10/cointoss.sol
@@ -10,14 +10,27 @@ The protocol must be secure, i.e., whenever a party deviates from the protocol i
 */
 
 contract cointoss {
-    address payable public player1;
-    address payable public player2;
+    address public player1;
+    address public player2;
     uint256 public betamount;
-    uint256 choice; //1 is head tails is 0
+    uint256 public choice; //1 is head tails is 0
     uint256 public result = 2;
     bytes32 commit;
     uint256 p2nonce;
     address owner;
+    struct flip {
+        uint256 choice;
+        address player1;
+        bytes32 commit;
+        address player2;
+        uint256 betamount;
+        uint256 nonce;
+        uint256 result;
+        uint256 expiration;
+    } 
+    
+    flip[] public flips;
+    mapping(address => flip) public games;
 
     constructor(uint256 p1choice, bytes32 secret) public payable {
         player1 = msg.sender;
@@ -27,21 +40,35 @@ contract cointoss {
         betamount = msg.value;
         //player1.transfer(value);
         commit = (secret);
+        flip storage game = games[msg.sender];
+        game.choice = choice;
+        game.player1 = msg.sender;
+        game.commit = commit;
+        game.betamount = betamount;
+        log3(
+            bytes32(uint256(msg.sender)),
+            bytes32(betamount),
+            bytes32(choice),
+            bytes32(secret)
+        );
         //commit = sha256(abi.encodePacked(secret));
     }
 
     uint256 public expiration = 2**256 - 1;
+    function () payable {}
 
     function takebet(uint256 nonce) public payable {
         require(player2 == address(0),"someone took the bet already"); //only 1 other player
         player2 = msg.sender;
-
+        games[player1].player2 = player2;
         //require(msg.value == betamount);
         require(msg.value == betamount,"betamount does not match"); //same bet amount
 
         //choice2 = choice2;
         expiration = now + 24 hours;
+        games[player1].expiration = expiration;
         p2nonce = nonce;
+        games[player1].nonce = nonce;
         //player2.transfer(va)
     }
 
@@ -53,13 +80,18 @@ contract cointoss {
         require((keccak256(abi.encodePacked(ran))) == commit,"hash mismatch");
 
         result = (ran + p2nonce) % 2; // returns 0 or 1
+        games[player1].result = result;
         if (result == choice) {
             player1.transfer(address(this).balance);
         } else {
             player2.transfer(address(this).balance);
         }
     }
-
+    function modifyNonce(uint256 nonce_) public {
+        require(result == 2, "reveal has already been called");
+        games[player1].nonce = nonce_;
+        p2nonce = nonce_;
+    }
     function claimTimeout() public payable {
         require(now >= expiration,"please wait for contract to expire");
         player2.transfer(address(this).balance);
@@ -70,6 +102,7 @@ contract cointoss {
         require(player2 == address(0),"someone has already joined the bet");
 
         betamount = 0;
+        games[player1].betamount = 0;
         msg.sender.transfer(address(this).balance);
     }
 }
